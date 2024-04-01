3a2b9665fac923f8b10a8b27102348cc3d0d38de
canongun <canongun90@gmail.com>
2024-04-01 17:23:07 +0200

    New ground truth listener service for base_link

---
+++ multirobot_services/nodes/ground_truth_listener_base_link_server.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+
+import rospy
+from nav_msgs.msg import Odometry
+from multirobot_services.srv import GroundTruthListener, GroundTruthListenerResponse
+
+class GroundTruthListenerBaseLinkNode:
+    """
+    The Multirobot Topic Listener
+    """
+
+    def __init__(self):
+        rospy.init_node('ground_truth_listener_base_link_server')
+
+        self.pose = Odometry()
+
+        self.message_received = False  # Flag to indicate whether a message has been received
+
+        rospy.Service('ground_truth_listener_base_link',    # service name
+                                GroundTruthListener,                # service type
+                                self.ground_truth_listener           # function service provides
+                                )
+
+    def callback(self, msg):
+        
+        self.pose = msg.pose
+        self.message_received = True  # Set the flag to True when a message is received
+
+    def ground_truth_listener(self, req):
+        
+        if req.start:
+
+            rospy.Subscriber('/ground_truth/base_link_state', Odometry, self.callback)
+
+            # Wait until a message is received before printing
+            while not self.message_received:
+                rospy.sleep(0.1)
+
+            link_info = []
+
+            link_info.extend([self.pose.pose.position.x,
+                            self.pose.pose.position.y,
+                            self.pose.pose.position.z,
+                            self.pose.pose.orientation.x,
+                            self.pose.pose.orientation.y,
+                            self.pose.pose.orientation.z,
+                            self.pose.pose.orientation.w])
+        
+            return GroundTruthListenerResponse(link_info)
+    
+    def run(self):
+        rate = rospy.Rate(20)
+        while not rospy.is_shutdown():
+            rate.sleep()
+            
+
+
+def main():
+    print("Listening to 'ground_truth/base_link_state' topic")
+    node = GroundTruthListenerBaseLinkNode()
+    node.run()
+    
+if __name__ == '__main__':
+    main()
